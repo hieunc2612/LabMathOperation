fab921a9367347e15a802c49a58d392880700ead
Hieu <nguyenconghieu@gmail.com>
2023-02-16 09:43:09 +0700

    Add project files.

---
+++ LabMathOperation/MathOperations.cs
@@ -0,0 +1,20 @@
+﻿namespace LabMathOperation
+{
+    internal class MathOperations
+    {
+
+        public int add(int a, int b) 
+        {
+            return a + b;
+        }
+        public double add(double a, double b,double c)
+        {
+            return a + b + c;
+        }
+        public decimal add(decimal a, decimal b,decimal c)
+        {
+            return a + b + c;
+        }
+
+    }
+}
